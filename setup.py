9db712d3cf6e400a11460a1e12b2885cc9c86b59
Steve Kieffer <sk@skieffer.info>
2022-06-26 18:50:14 -0400

    Repackage for v0.6.7

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
-import re
 from setuptools import setup
 
-__version__ ,= re.findall('__version__ = "(.*)"', open('lark/__init__.py').read())
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 setup(
-    name = "lark-parser",
-    version = __version__,
+    name = "lark067",
+    version = '0.6.7',
     packages = ['lark', 'lark.parsers', 'lark.tools', 'lark.grammars'],
 
     requires = [],
@@ -15,38 +15,13 @@ setup(
 
     test_suite = 'tests.__main__',
 
-    # metadata for upload to PyPI
     author = "Erez Shinan",
-    author_email = "erezshin@gmail.com",
     description = "a modern parsing library",
     license = "MIT",
     keywords = "Earley LALR parser parsing ast",
-    url = "https://github.com/erezsh/lark",
-    download_url = "https://github.com/erezsh/lark/tarball/master",
-    long_description='''
-Lark is a modern general-purpose parsing library for Python.
-
-With Lark, you can parse any context-free grammar, efficiently, with very little code.
-
-Main Features:
- - Builds a parse-tree (AST) automagically, based on the structure of the grammar
- - Earley parser
-    - Can parse all context-free grammars
-    - Full support for ambiguous grammars
- - LALR(1) parser
-    - Fast and light, competitive with PLY
-    - Can generate a stand-alone parser
- - CYK parser, for highly ambiguous grammars
- - EBNF grammar
- - Unicode fully supported
- - Python 2 & 3 compatible
- - Automatic line & column tracking
- - Standard library of terminals (strings, numbers, names, etc.)
- - Import grammars from Nearley.js
- - Extensive test suite
- - And much more!
-''',
-
+    url = "https://github.com/skieffer/lark",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
